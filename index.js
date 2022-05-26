264e8e423f26c1d1215f54b70ae1ef95b5dd3544
dawhitoo <88205069+dawhitoo@users.noreply.github.com>
2022-05-26 16:14:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
-function calcularEdad(fecha) {
-    var hoy = new Date();
-    var cumpleaÑos = new Date(fecha);
-    var edad = hoy.getFullYear() - cumpleaÑos.getFullYear();
-    var m = hoy.getMonth() - cumpleaÑos.getMonth();
 
-    if (m < 0 || (m === 0 && hoy.getDate() < cumpleaÑos.getDate())) {
-        edad--;
-    }
-
-    return edad;
-}
+	document.oncontextmenu = function(){return false}
